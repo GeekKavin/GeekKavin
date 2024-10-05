cd2908eb6eef7a2dae9e38de8aa30486e9f07398
GeekKavin <162860440+GeekKavin@users.noreply.github.com>
2024-10-05 17:49:17 +0530

    Add files via upload

---
+++ MergeAlternateStrings.java
@@ -0,0 +1,27 @@
+public class MergeAlternateStrings {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder mergedAlternatives = new StringBuilder();
+        int i = 0, j = 0;
+
+
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                mergedAlternatives.append(word1.charAt(i++));
+            }
+            if (j < word2.length()) {
+                mergedAlternatives.append(word2.charAt(j++));
+            }
+        }
+            return mergedAlternatives.toString();
+
+        }
+
+        public static void main (String[]args){
+            MergeAlternateStrings m = new MergeAlternateStrings();
+            String word1 = "kavi";
+            String word2 = "chithan";
+            String result = m.mergeAlternately(word1, word2);
+            System.out.println(result);
+        }
+    }
+
